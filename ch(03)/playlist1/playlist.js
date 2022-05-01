d517719f5d3b9b1f69b179b7851f36bbf73056ca
YunA486 <w2039@e-mirim.hs.kr>
2022-05-02 00:48:42 +0900

    ch(03) playlist1 곡 전체 삭제

---
+++ ch(03)/playlist1/playlist.js
@@ -0,0 +1,42 @@
+/* playlist.js */
+
+window.onload = init;
+
+function init() {
+	var button = document.getElementById("addButton");
+	button.onclick = handleButtonClick;
+
+	// 추가1
+	var button = document.getElementById("deleteAllButton");
+	button.onclick = handleButtonClick1;
+	loadPlaylist();
+	// playlist_store에 있는 loadPlaylist 함수
+}
+
+function handleButtonClick(e) {
+	var textInput = document.getElementById("songTextInput");
+	var songName = textInput.value;
+	//alert("Adding " + songName);
+
+	if (songName == "") {
+		alert("곡을 입력하세요");
+	} else {
+		var li = document.createElement("li");
+		li.innerHTML = songName;
+		var ul = document.getElementById("playlist");
+		ul.appendChild(li);
+		save(songName);
+	}
+}
+
+//추가1
+function handleButtonClick1(e) {
+	removeAll();
+}
+
+function removeAll() {
+	if (confirm('모두 지울까요?')) {
+		localStorage.clear();
+		// 전체 삭제
+	}
+}
